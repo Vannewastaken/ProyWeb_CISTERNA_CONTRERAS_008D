ce6063e4d555f550d60852086d17c928dc00c9b1
Vannewastaken <valentinacisterna2004@gmail.com>
2024-05-11 02:23:42 -0400

    11-05

--- JS/FUNCION.js
+++ JS/FUNCION.js
@@ -35,3 +35,59 @@ function iniciarSesion(){
     }
 
 }
+
+
+// LUKAS REGISTRO
+
+function registrarUsuario(){
+    let nom= document.getElementById('name').value;
+    let usu= document.getElementById('uname').value;
+    let em= document.getElementById('email').value;
+    let tel= document.getElementById('telefono').value;
+     let con= document.getElementById('contraseña').value;
+    let concon= document.getElementById('confirmarContraseña').value;
+
+    em=em.toLowerCase();
+    const validar = expresion.test(em);
+
+
+    if (nom.length < 20) {
+        alert('Error..debe ingresar el nombre completo');
+        document.getElementById('name').value = "";
+        document.getElementById('name').focus();
+    }
+    else if (usu.length < 4) {
+         alert('Error..debe ingresar un nombre de usuario mayor a 3 caracteres');
+         document.getElementById('uname').value = "";
+         document.getElementById('uname').focus();
+        
+     }
+     else if(validar==false){
+        alert('El correo electronico no es valido');
+        document.getElementById('email').value = "";
+        document.getElementById('email').focus();
+
+    }
+    else if (tel.length < 8) {
+        alert('Error..debe ingresar un telefono correcto');
+        document.getElementById('telefono').value = "";
+        document.getElementById('telefono').focus();
+    }
+    else if (con.length < 8) {
+        alert('Error..debe ingresar una contraseña de 8 caracteres o más');
+         document.getElementById('contraseña').value = "";
+         document.getElementById('contraseña').focus();
+     }
+     else if (concon !== con) {
+        alert('Error..las contraseñas no coinciden');
+        document.getElementById('contraseña').value = "";
+        document.getElementById('confirmarContraseña').value = "";
+        document.getElementById('contraseña').focus();
+    }
+    else{
+        alert('Te has registrado correctamente!')
+    }
+    
+}
+
+
